6b15c76411652bd9b2a35e0da97d506b7d2bc956
gabriel <gabriel.piercecchi@gmail.com>
2025-01-14 10:09:19 +0100

    Crazione status delle organizzazioni ed employers

--- database/seeder.py
+++ database/seeder.py
@@ -31,10 +31,10 @@ def seed_types(session: Session):
 def seed_organizations(session: Session):
     if not session.query(Organization).first():
         organizations = [
-            Organization(name='Org1', ragione_sociale='RS1', description='Desc1', partita_iva='12345678901', address='Address1', city='City1', cap='00001', telephone='1234567890', email='org1@example.com', type='farmer', coin=200.0),
-            Organization(name='Org2', ragione_sociale='RS2', description='Desc2', partita_iva='12345678902', address='Address2', city='City2', cap='00002', telephone='1234567891', email='org2@example.com', type='seller', coin=200.0),
-            Organization(name='Org3', ragione_sociale='RS3', description='Desc3', partita_iva='12345678903', address='Address3', city='City3', cap='00003', telephone='1234567892', email='org3@example.com', type='carrier', coin=200.0),
-            Organization(name='Org4', ragione_sociale='RS4', description='Desc4', partita_iva='12345678904', address='Address4', city='City4', cap='00004', telephone='1234567893', email='org4@example.com', type='carrier', coin=200.0),
+            Organization(name='Org1', ragione_sociale='RS1', description='Desc1', partita_iva='12345678901', address='Address1', city='City1', cap='00001', telephone='1234567890', email='org1@example.com', type='farmer', status= 'active', coin=200.0),
+            Organization(name='Org2', ragione_sociale='RS2', description='Desc2', partita_iva='12345678902', address='Address2', city='City2', cap='00002', telephone='1234567891', email='org2@example.com', type='seller', status= 'active', coin=200.0),
+            Organization(name='Org3', ragione_sociale='RS3', description='Desc3', partita_iva='12345678903', address='Address3', city='City3', cap='00003', telephone='1234567892', email='org3@example.com', type='carrier', status= 'active', coin=200.0),
+            Organization(name='Org4', ragione_sociale='RS4', description='Desc4', partita_iva='12345678904', address='Address4', city='City4', cap='00004', telephone='1234567893', email='org4@example.com', type='carrier', status= 'active', coin=200.0),
         ]
         session.bulk_save_objects(organizations)
         session.commit()
@@ -42,10 +42,10 @@ def seed_organizations(session: Session):
 def seed_employers(session: Session):
     if not session.query(Employer).first():
         employers_data = [
-            {'username': 'user1', 'password': 'pass1', 'name': 'Name1', 'surname': 'Surname1', 'email': 'email1@example.com', 'id_organization': 1},
-            {'username': 'user2', 'password': 'pass2', 'name': 'Name2', 'surname': 'Surname2', 'email': 'email2@example.com', 'id_organization': 2},
-            {'username': 'user3', 'password': 'pass3', 'name': 'Name3', 'surname': 'Surname3', 'email': 'email3@example.com', 'id_organization': 3},
-            {'username': 'user4', 'password': 'pass4', 'name': 'Name4', 'surname': 'Surname4', 'email': 'email4@example.com', 'id_organization': 4},
+            {'username': 'user1', 'password': 'pass1', 'name': 'Name1', 'surname': 'Surname1', 'email': 'email1@example.com', 'status': 'active', 'id_organization': 1},
+            {'username': 'user2', 'password': 'pass2', 'name': 'Name2', 'surname': 'Surname2', 'email': 'email2@example.com', 'status': 'active', 'id_organization': 2},
+            {'username': 'user3', 'password': 'pass3', 'name': 'Name3', 'surname': 'Surname3', 'email': 'email3@example.com', 'status': 'active', 'id_organization': 3},
+            {'username': 'user4', 'password': 'pass4', 'name': 'Name4', 'surname': 'Surname4', 'email': 'email4@example.com', 'status': 'active', 'id_organization': 4},
         ]
         employers = []
         for data in employers_data:
@@ -54,6 +54,7 @@ def seed_employers(session: Session):
                 name=data['name'],
                 surname=data['surname'],
                 email=data['email'],
+                status=data['status'],
                 id_organization=data['id_organization']
             )
             employer.set_password(data['password'])  # Hash the password
