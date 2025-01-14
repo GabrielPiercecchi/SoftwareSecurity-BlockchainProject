eca1dbf5e88b7128265c92f541b3431c4b9ba00e
tosca <toscapierro02@gmail.com>
2025-01-14 10:28:10 +0100

    manage_organization verifica

--- controllers/oracle_controller.py
+++ controllers/oracle_controller.py
@@ -1,6 +1,6 @@
 from flask import session, redirect, url_for, render_template
 from database.database import DBIsConnected
-from database.migration import Oracle
+from database.migration import Oracle, Organization
 
 def oracle_home():
     username = session.get('username')
@@ -20,6 +20,6 @@ def manage_organization_registration():
     
     db_instance = DBIsConnected.get_instance()
     session_db = db_instance.get_session()
-    oracle_user = session_db.query(Oracle).filter_by(username=username).first()
+    pending_organizations = session_db.query(Organization).filter_by(status='pending').all()
     session_db.close()
-    return render_template('oracle_manage_organization_registration.html', oracle=oracle_user)
+    return render_template('oracle_manage_organization_registration.html', pending_organization=pending_organizations)
